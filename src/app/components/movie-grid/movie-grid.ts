1e3cdeee8f89fe14e9a806f8a87640ab6dd39ba7
diego <diegosorianomagallon@gmail.com>
2025-10-25 14:50:06 +0200

    New Movie Grid

--- src/app/components/movie-grid/movie-grid.ts
+++ src/app/components/movie-grid/movie-grid.ts
@@ -1,11 +1,9 @@
 import { Component, effect, inject, input } from '@angular/core';
-import { NgIcon, provideIcons } from "@ng-icons/core";
+import { Router, RouterLink } from '@angular/router';
+import { NgIcon, provideIcons } from '@ng-icons/core';
 import { phosphorFilmSlateFill } from '@ng-icons/phosphor-icons/fill';
 import { phosphorPlus } from '@ng-icons/phosphor-icons/regular';
-import { Router, RouterLink } from "@angular/router";
 import { Movie, MovieDetails } from '../../models/movie.models';
-import { StorageService } from '../../services/storage-service/storage-service';
-
 
 @Component({
   selector: 'app-movie-grid',
@@ -15,23 +13,28 @@ import { StorageService } from '../../services/storage-service/storage-service';
   viewProviders: [provideIcons({ phosphorPlus, phosphorFilmSlateFill })]
 })
 export class MovieGrid {
-  private storageService = inject(StorageService)
   private router = inject(Router)
 
-  movies = this.storageService.savedMovies
+  movies = input.required<(Movie | MovieDetails)[]>()
 
-  onMovieSelect(movieId: number): void {
+  constructor() {
+    effect(() => {
+      console.log('🎬 MovieGrid - Películas recibidas:', this.movies());
+    })
+  }
+
+  goToMovieDetails(movieId: number): void {
     this.router.navigate(['/movie', movieId])
   }
 
   getPosterUrl(movie: Movie | MovieDetails): string {
     if (movie?.poster_path) {
-      return `https://image.tmdb.org/t/p/w500${movie.poster_path}`
+      return `https://image.tmdb.org/t/p/w500${movie.poster_path}`;
     }
-    return ''
+    return '';
   }
 
   hasPoster(movie: Movie | MovieDetails): boolean {
-    return !!movie?.poster_path
+    return !!movie?.poster_path;
   }
 }
--- src/app/pages/home-page/home-page.ts
+++ src/app/pages/home-page/home-page.ts
@@ -1,14 +1,15 @@
-import { Component, inject, signal } from '@angular/core';
+import { Component, effect, inject, signal } from '@angular/core';
 import { NgIcon, provideIcons } from "@ng-icons/core";
 import { phosphorFunnelSimple, phosphorMagnifyingGlass } from '@ng-icons/phosphor-icons/regular'
 import { phosphorMonitorPlayFill } from '@ng-icons/phosphor-icons/fill'
-import { MovieGrid } from "../../components/movie-grid/movie-grid";
 import { StorageService } from '../../services/storage-service/storage-service';
 import { SortMenuComponent } from "../../components/sort-menu-component/sort-menu-component";
+import { MovieGrid } from "../../components/movie-grid/movie-grid";
+import { SearchBar } from "../../components/search-bar/search-bar";
 
 @Component({
   selector: 'app-home-page',
-  imports: [NgIcon, MovieGrid, SortMenuComponent],
+  imports: [NgIcon, SortMenuComponent, MovieGrid, SearchBar],
   templateUrl: './home-page.html',
   styleUrl: './home-page.css',
   viewProviders: [provideIcons({ phosphorFunnelSimple, phosphorMagnifyingGlass, phosphorMonitorPlayFill })]
@@ -18,4 +19,13 @@ export class HomePage {
 
   savedMovies = this.storageService.savedMovies
   moviesCount = signal(this.savedMovies().length)
+
+  constructor() {
+    console.log('savedMovies():', this.savedMovies())
+
+    effect(() => {
+      const movies = this.savedMovies()
+      console.log(movies)
+    })
+  }
 }
