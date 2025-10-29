44f018fbf50bc9fb8350153cd556c254b0bd86ee
diegosorianom <156294535+diegosorianom@users.noreply.github.com>
2025-10-29 19:44:58 +0100

    sort results

--- src/app/components/search-results/search-results.ts
+++ src/app/components/search-results/search-results.ts
@@ -1,4 +1,4 @@
-import { Component, input, output } from '@angular/core';
+import { Component, computed, input, output } from '@angular/core';
 import { Movie } from '../../models/movie.models';
 import { NgIcon, provideIcons } from "@ng-icons/core";
 import { phosphorArrowClockwiseLight } from '@ng-icons/phosphor-icons/light';
@@ -17,6 +17,19 @@ export class SearchResults {
   isLoading = input<boolean>(false)
   error = input<string | null>(null)
 
+   filteredResults = computed(() => {
+    const results = this.searchResults().filter(movie => 
+      movie.backdrop_path || movie.poster_path
+    );
+    
+    return results.sort((a, b) => {
+      const dateA = a.release_date ? new Date(a.release_date).getTime() : Number.MAX_SAFE_INTEGER;
+      const dateB = b.release_date ? new Date(b.release_date).getTime() : Number.MAX_SAFE_INTEGER;
+      
+      return dateA - dateB;
+    });
+  });
+
   movieSelected = output<number>()
   clearSearch = output<void>()
 
@@ -26,5 +39,21 @@ export class SearchResults {
 
   onClearSearch(): void {
     this.clearSearch.emit()
+  }
+
+  getBackdropUrl(backdropPath: string | null): string {
+    if (backdropPath) {
+      return `https://image.tmdb.org/t/p/w780${backdropPath}`;
+    } else {
+      return '/assets/placeholder-backdrop.jpg';
+    }
+  }
+
+  getPosterUrl(posterPath: string | null): string {
+    if (posterPath) {
+      return `https://image.tmdb.org/t/p/w92${posterPath}`;
+    } else {
+      return '/assets/placeholder-movie.jpg';
+    }
   }
 }
